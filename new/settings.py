b68953f0a3a929f9303839c25f9d360341c4c03e
Bishtu <bistudas.das@gmail.com>
2025-10-23 01:32:40 +0530

    about page

--- new/settings.py
+++ new/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6+*seicynbp))_u%sq76-fjy(i)w5v+@vp+&z1!6v%)6$lv@i&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
